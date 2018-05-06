b1c1bc7e4964532d54fbc0c52c48b517f8c587fd
Luis Henrique de Oliveira Amorim <lamorim@scopus.com.br>
2018-05-06 09:22:19 -0300

    create network controlller

---
+++ WeatherApp/Networking/NetworkController.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkController.swift
+//  WeatherApp
+//
+//  Created by Luis Henrique de Oliveira Amorim on 5/6/18.
+//  Copyright © 2018 Nyisztor, Karoly. All rights reserved.
+//
+
+import Foundation
+
+public protocol NetworkController{
+    func fetchCurrentWeatherData(city:String) -> WeatherData?
+}
+
+public struct WeatherData {
+    var temperature:Float
+    var condition:String
+    var unit:TemperatureUnit
+}
+
+public enum TemperatureUnit:String{
+    case scientific = "scientific"
+    case metric = "metric"
+    case imperial = "imperial"
+}
